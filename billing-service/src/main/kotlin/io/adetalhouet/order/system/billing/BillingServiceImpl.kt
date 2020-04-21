16f76f3f2c9e4f40e1b1db1f22d256ff4a4f9601
Alexis de Talhouët <adetalhouet89@gmail.com>
2020-04-20 22:32:52 -0400

    Test graphql to grpc wiring
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

--- billing-service/src/main/kotlin/io/adetalhouet/order/system/billing/BillingServiceImpl.kt
+++ billing-service/src/main/kotlin/io/adetalhouet/order/system/billing/BillingServiceImpl.kt
@@ -1,11 +1,15 @@
 package io.adetalhouet.order.system.billing
 
 import io.adetalhouet.order.system.utils.Status
+import org.slf4j.LoggerFactory
 
 class BillingServiceImpl : BillingServiceGrpcKt.BillingServiceCoroutineImplBase() {
 
+    val log = LoggerFactory.getLogger(BillingServiceImpl::class.java)
+
     override suspend fun addBill(request: Bill): Status {
-        return super.addBill(request)
+        log.info("Add bill")
+        return Status.newBuilder().setStatus(Status.State.SUCCESS).build()
     }
 
     override suspend fun getBillByClient(request: GetBillByClientRequest): Bills {
